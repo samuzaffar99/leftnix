488830aa8c3272b60171cbd4a7bd82ce384aaf3e
Syed Abdullah Muzaffar <56280132+samuzaffar99@users.noreply.github.com>
2022-04-14 19:17:18 +0500

    add controller to loginui

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:leftnix/chain.dart';
+import 'package:leftnix/secrets.dart';
 import 'package:leftnix/widgets.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
@@ -8,18 +10,9 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Get.put(LoginController());
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomCenter,
-          stops: [0.2, 0.6],
-          colors: [
-            Colors.red,
-            secondaryColor,
-          ],
-        ),
-      ),
+      decoration: backgroundDecor,
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
@@ -37,12 +30,13 @@ class LoginPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                const LoginForm(),
+                const Text("OR"),
                 CustomButton(
-                  prefixIcon: const Icon(Icons.exit_to_app),
-                  onPressed: () => Get.toNamed("/"),
-                  labelText: "Login",
-                ),
-                // const Banner(),
+                  prefixIcon: const Icon(Icons.app_registration),
+                  onPressed: () => Get.toNamed("/register"),
+                  labelText: "Register Now",
+                )
               ],
             ),
           ),
@@ -52,28 +46,47 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class LoginForm extends StatelessWidget {
+class LoginController extends GetxController {
+  final loginForm = fb.group(
+    {
+      "key": [
+        privateKey,
+        Validators.required,
+        Validators.minLength(32),
+        Validators.maxLength(84),
+      ],
+    },
+  );
+
+  Future<void> login() async {
+    String key = loginForm.value["key"] as String;
+    String username = loginForm.value["username"] as String;
+    final Session network = Get.find<Session>();
+    bool existing = await network.register(key,username);
+    if (existing) {
+      Get.offAllNamed("/home");
+    } else {
+      print("User does not exist!");
+    }
+  }
+}
+
+class LoginForm extends GetView<LoginController> {
   const LoginForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final signinForm = fb.group({
-      "key": [
-        "",
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(64),
-      ],
-    });
     return ReactiveForm(
-      formGroup: signinForm,
+      formGroup: controller.loginForm,
       child: Column(
         children: [
-          TextFormField(),
-          TextFormField(),
           const CustomPasswordTextField(
             formControlName: 'key',
             labelText: "Key",
+          ),
+          ConsumerCustomButton(
+            onValidPressed: () => controller.login(),
+            labelText: "Login",
           ),
         ],
       ),
